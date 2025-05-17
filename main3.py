234c3db9b75f74df64d7b8576c35bd91a3f995b1
Tia201427 <tia.anand279@gmail.com>
2025-05-17 11:43:52 +0530

    datatype

---
+++ main3.py
@@ -0,0 +1,9 @@
+a = int(input("enter a number"))
+b = int(input("enter a number"))
+c = int(input("enter a number"))
+d = int(input("enter a number"))
+e = int(input("enter a number"))
+sum = a+b+c+d+e
+print(sum)
+avg = sum / 5
+print(avg)
---
+++ main4.py
@@ -0,0 +1,7 @@
+geography = int(input("enter your marks"))
+history = int(input("enter your marks"))
+math = int(input("enter your marks"))
+sum = geography + history + math 
+print(sum)
+per = (sum / 300)*100
+print(per)
